d07b69da927633ada75712b043ae92865c8beb83
NewUser <newuser@example.com>
2013-11-15 17:58:32 +0530

    first

---
+++ WeatherApp/ShowWheatherViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShowWheatherViewController.h
+//  WeatherApp
+//
+//  Created by Raj Kadam on 13/11/13.
+//  Copyright (c) 2013 Raj kadam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShowWheatherViewController : UIViewController
+
+@end
---
+++ WeatherApp/ShowWheatherViewController.m
@@ -0,0 +1,38 @@
+//
+//  ShowWheatherViewController.m
+//  WeatherApp
+//
+//  Created by Raj Kadam on 13/11/13.
+//  Copyright (c) 2013 Raj kadam. All rights reserved.
+//
+
+#import "ShowWheatherViewController.h"
+
+@interface ShowWheatherViewController ()
+
+@end
+
+@implementation ShowWheatherViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
